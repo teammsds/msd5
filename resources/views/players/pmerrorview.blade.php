5764b4c8749fdb66da4d1e5d9d94bb4c1f436277
TarunReddyBattula <tarunreddy.battula@gmail.com>
2017-04-25 21:29:40 -0500

    first commit

---
+++ resources/views/players/pmerrorview.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-15 col-md-offset-0">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 style= "color: black">Player matches does not exist</h4>
+
+
+         
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/players/pperrorview.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-15 col-md-offset-0">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 style= "color: black"> Player profile does not exist</h4>
+
+
+         
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
